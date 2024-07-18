c69ef17cd7d6c52a653a2ee3bee4e0837e9547eb
dimar1150 <158440383+Dimar1510@users.noreply.github.com>
2024-07-18 15:14:03 +0300

    mongoose and mock data setup

--- server/app.js
+++ server/app.js
@@ -5,10 +5,10 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
 const helmet = require("helmet");
-const indexRouter = require("./routes/index");
+const kpiRoutes = require("./routes/kpi");
 const cors = require("cors");
-require("dotenv").config();
 
+require("dotenv").config();
 const app = express();
 
 app.use(logger("dev"));
@@ -21,7 +21,7 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+app.use("/kpi", kpiRoutes);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ server/models/KPI.js
@@ -0,0 +1,87 @@
+const mongoose = require("mongoose");
+require("mongoose-currency").loadType(mongoose);
+
+const Schema = mongoose.Schema;
+
+const monthSchema = new Schema(
+  {
+    month: String,
+    revenue: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100,
+    },
+    expenses: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100,
+    },
+    operationalExpenses: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100,
+    },
+    nonOperationalExpenses: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100,
+    },
+  },
+  { toJSON: { getters: true } }
+);
+
+const daySchema = new Schema(
+  {
+    day: String,
+    revenue: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100,
+    },
+    expenses: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100,
+    },
+  },
+  { toJSON: { getters: true } }
+);
+
+const KPISchema = new Schema(
+  {
+    totalProfit: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100,
+    },
+    totalRevenue: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100,
+    },
+    totalExpenses: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100,
+    },
+    expenseByCategory: {
+      type: Map,
+      of: {
+        type: mongoose.Types.Currency,
+        currency: "USD",
+        of: {
+          type: mongoose.Types.Currency,
+          currency: "USD",
+          get: (v) => v / 100,
+        },
+      },
+    },
+    monthlyData: [monthSchema],
+    dailyData: [daySchema],
+  },
+  { timestamps: true, toJSON: { getters: true } }
+);
+
+const KPI = mongoose.model("KPI", KPISchema);
+
+module.exports = KPI;
