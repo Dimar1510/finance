4b85de784a4489d9f0912169e3f5ee31bca66ed2
dimar1150 <158440383+Dimar1510@users.noreply.github.com>
2024-07-18 11:50:54 +0300

    router and navigation

---
+++ client/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,57 @@
+import { Box, Typography, useTheme } from "@mui/material";
+import { useState } from "react";
+import FlexBetween from "src/components/ui/FlexBetween";
+import { Pix } from "@mui/icons-material";
+import { NavLink } from "react-router-dom";
+const Navbar = () => {
+  const { palette } = useTheme();
+  const [selected, setSelected] = useState("");
+  return (
+    <FlexBetween mb={"0.25rem"} p={"0.5rem 0rem"} color={palette.grey[300]}>
+      <NavLink
+        to={"/"}
+        onClick={() => setSelected("dashboard")}
+        style={{
+          color: "inherit",
+          textDecoration: "inherit",
+        }}
+      >
+        <FlexBetween gap={"0.75rem"}>
+          <Pix sx={{ fontSize: "28px" }} />
+          <Typography variant="h4" fontSize={"16px"}>
+            Finanseer
+          </Typography>
+        </FlexBetween>
+      </NavLink>
+
+      <FlexBetween gap={"2rem"}>
+        <Box sx={{ "&:hover": { color: palette.primary[100] } }}>
+          <NavLink
+            to={"/"}
+            onClick={() => setSelected("dashboard")}
+            style={{
+              color: selected === "dashboard" ? "inherit" : palette.grey[700],
+              textDecoration: "inherit",
+            }}
+          >
+            Dashboard
+          </NavLink>
+        </Box>
+        <Box>
+          <NavLink
+            to={"/predictions"}
+            onClick={() => setSelected("predictions")}
+            style={{
+              color: selected === "predictions" ? "inherit" : palette.grey[700],
+              textDecoration: "inherit",
+            }}
+          >
+            Predictions
+          </NavLink>
+        </Box>
+      </FlexBetween>
+    </FlexBetween>
+  );
+};
+
+export default Navbar;
